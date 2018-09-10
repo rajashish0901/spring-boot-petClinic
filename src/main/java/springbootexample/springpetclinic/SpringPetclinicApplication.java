de2e71b3514898454e50bf181ab5378ad3525f1b
Ashish Raj <ashish.raj@mobiliya.com>
2018-09-10 15:15:47 +0530

    initial project add.

---
+++ src/main/java/springbootexample/springpetclinic/SpringPetclinicApplication.java
@@ -0,0 +1,12 @@
+package springbootexample.springpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPetclinicApplication.class, args);
+    }
+}
