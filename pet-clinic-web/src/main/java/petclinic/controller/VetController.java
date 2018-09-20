9fec8811ec59559f0ef8a78fbc157dd5deb9c76c
Ashish Raj <ashish.raj@mobiliya.com>
2018-09-20 18:26:40 +0530

    Added fetching VetList

--- pet-clinic-web/src/main/java/petclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/petclinic/controller/VetController.java
@@ -1,13 +1,23 @@
 package petclinic.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import petclinic.services.VetService;
 
 @Controller
 public class VetController {
+    VetService m_vetService;
+
+    public VetController(VetService vetService) {
+        System.out.println(" @@ VetController(VetService vetService) @@ ");
+        this.m_vetService = vetService;
+    }
 
     @RequestMapping({"vets","vets/index","vets/index.html"})
-    public String listVets(){
+    public String listVets(Model model){
+        System.out.println(" @@ listVets(Model model) @@ ");
+        model.addAttribute("vets", m_vetService.findAll());
         return "vets/index";
     }
 }
