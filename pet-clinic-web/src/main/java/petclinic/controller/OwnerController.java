bf583f4d6f82ece516c2c76b2eae0f4541a7cf3e
Ashish Raj <ashish.raj@mobiliya.com>
2018-09-20 18:02:41 +0530

    Added fetching OwnerList

--- pet-clinic-web/src/main/java/petclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/petclinic/controller/OwnerController.java
@@ -1,13 +1,24 @@
 package petclinic.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import petclinic.services.OwnerService;
 
 @Controller
 public class OwnerController {
 
+    OwnerService m_ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        System.out.println(" @@ listOwners(Model model) @@ ");
+        this.m_ownerService = ownerService;
+    }
+
     @RequestMapping({"owners","owners/index","owners/index.html"})
-    public String listOwners(){
+    public String listOwners(Model model){
+        System.out.println(" @@ listOwners(Model model) @@ ");
+        model.addAttribute("owners", m_ownerService.findAll());
         return "owners/index";
     }
 }
