9db4b19af10e98edf8752f658319b674842246df
Ashish Raj <ashish.raj@mobiliya.com>
2018-09-24 17:58:49 +0530

    Added contact details to Owner module.

--- pet-clinic-data/src/main/java/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/petclinic/model/Owner.java
@@ -3,7 +3,35 @@ package petclinic.model;
 import java.util.Set;
 
 public class Owner extends Person {
+    private String address;
+    private String telephone;
+    private String city;
+
     Set<Pet> m_pet;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
 
     public Set<Pet> getM_pet() {
         return m_pet;
