2657b273a9d40b8020e06fe5a9a7de21a950850e
Ashish Raj <ashish.raj@mobiliya.com>
2018-09-24 17:55:17 +0530

    Added SpecialitySet

---
+++ pet-clinic-data/src/main/java/petclinic/model/Speciality.java
@@ -0,0 +1,15 @@
+package petclinic.model;
+
+import petclinic.services.BaseEntity;
+
+public class Speciality extends BaseEntity {
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
